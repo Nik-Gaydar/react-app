eb13e04fbef380a547162fe56fdfcf23c0547fe9
Nik-Gaydar <nik-gaydar@yandex.ru>
2025-11-25 21:15:32 +0700

    Added props in component

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,11 +1,15 @@
-import Field from "./Field.jsx";
-import Button from "./Button.jsx";
+import Field from "./Field";
+import Button from "./Button";
 
 const AddTaskForm = () => {
   return (
     <form className="todo__form">
-      <Field />
-      <Button />
+      <Field
+        className="todo__field"
+        label="New task title"
+        id="new-task"
+      />
+      <Button type="submit">Add</Button>
     </form>
   )
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,24 @@
-import AddTaskForm from "./AddTaskForm.jsx";
-import SearchTaskForm from "./SearchTaskForm.jsx";
-import TodoInfo from "./TodoInfo.jsx";
-import TodoList from "./TodoList.jsx";
+import AddTaskForm from "./AddTaskForm";
+import SearchTaskForm from "./SearchTaskForm";
+import TodoInfo from "./TodoInfo";
+import TodoList from "./TodoList";
 
 const Todo = () => {
+  const tasks = [
+    {id: 'task-1', title: 'Купить ноутбук', isDone: false},
+    {id: 'task-2', title: 'Закрыть долги', isDone: true},
+  ]
+
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
       <AddTaskForm />
       <SearchTaskForm />
-      <TodoInfo />
-      <TodoList />
+      <TodoInfo
+        total={tasks.length}
+        done={tasks.filter(({isDone}) => isDone).length}
+      />
+      <TodoList tasks={tasks}/>
     </div>
   )
 }
