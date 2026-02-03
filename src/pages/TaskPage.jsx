18eb76e85a62e38059b0ccf7b3e657c60c52fa7d
Nik_Gaydar <nikgaydar@yandex.ru>
2026-02-03 15:49:01 +0700

    routing WIP

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,7 +1,39 @@
+import {useEffect, useState} from "react";
+import tasksAPI from "../api/TasksAPI.js";
+
 const TaskPage = () => {
+  const taskId = 'id'
+
+  const [task, setTask] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHaserror] = useState(false)
+
+  useEffect(() => {
+    tasksAPI.getById(taskId)
+      .then((taskData) => {
+        setTask(taskData)
+        setHaserror(false)
+      })
+      .catch(() => {
+        setHaserror(true)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (hasError) {
+    return <div>Task not found!</div>
+  }
+
   return (
     <div>
-      <h1>Детали задачи</h1>
+      <h1>{task.title}</h1>
+      <p>{task.isDone ? 'Задача выполнена' : 'Задача не выполнена'}</p>
     </div>
   )
 }
