2079e657acbf2fa64ecc4e4d87284fbe26417a9b
Nik_Gaydar <nikgaydar@yandex.ru>
2026-02-05 15:47:40 +0700

    FSD WIP

---
+++ src/app/App.jsx
@@ -0,0 +1,19 @@
+import Router from './routing/Router'
+import TasksPage from '@/pages/TasksPage'
+import TaskPage from '@/pages/TaskPage'
+import './styles'
+
+
+const App = () => {
+  const routes = {
+    '/': TasksPage,
+    '/tasks/123': TaskPage,
+    '*': () => <div>404 Page not found</div>
+  }
+
+  return (
+    <Router routes={routes}/>
+  )
+}
+
+export default App
---
+++ src/entities/todo/ui/TodoList/TodoList.jsx
@@ -0,0 +1,37 @@
+import { memo, useContext } from 'react'
+import TodoItem from "@/TodoItem/TodoItem.jsx";
+import { TasksContext } from "@/model/TasksContext.jsx";
+
+const TodoList = (props) => {
+  const {styles} = props
+
+  const {
+    tasks,
+    filteredTasks,
+  } = useContext(TasksContext)
+
+  const hasTasks = tasks.length > 0
+  const isEmptyFilteredTasks = filteredTasks?.length === 0
+
+  if (!hasTasks) {
+    return <div className={styles.emptyMessage}>Задач пока нет</div>
+  }
+
+  if (hasTasks && isEmptyFilteredTasks) {
+    return <div className={styles.emptyMessage}>Задачи не найдены</div>
+  }
+
+  return (
+    <ul className={styles.list}>
+      {(filteredTasks ?? tasks).map((task) => (
+        <TodoItem
+          className={styles.item}
+          key={task.id}
+          {...task}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export default memo(TodoList)
