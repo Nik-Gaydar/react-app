5a7e2fcb6debabd9f4ccaffbe1d52be5bf92f1af
Nik-Gaydar <nik-gaydar@yandex.ru>
2026-02-07 17:10:30 +0700

    useReducer

--- src/entities/todo/model/TasksContext.jsx
+++ src/entities/todo/model/TasksContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react'
+import { createContext, useMemo } from 'react'
 import useTasks from "./useTasks.js";
 import useIncompleteTaskScroll from "./useIncompleteTaskScroll.js";
 
@@ -28,26 +28,42 @@ export const TasksProvider = (props) => {
     firstIncompleteTaskRef,
   } = useIncompleteTaskScroll(tasks)
 
+  const value = useMemo(() => ({
+    tasks,
+    filteredTasks,
+    deleteTask,
+    deleteAllTasks,
+    toggleTaskComplete,
+    newTaskTitle,
+    setNewTaskTitle,
+    searchQuery,
+    setSearchQuery,
+    newTaskInputRef,
+    addTask,
+    disappearingTaskId,
+    appearingTaskId,
+    firstIncompleteTaskId,
+    firstIncompleteTaskRef,
+  }), [
+    tasks,
+    filteredTasks,
+    deleteTask,
+    deleteAllTasks,
+    toggleTaskComplete,
+    newTaskTitle,
+    setNewTaskTitle,
+    searchQuery,
+    setSearchQuery,
+    newTaskInputRef,
+    addTask,
+    disappearingTaskId,
+    appearingTaskId,
+    firstIncompleteTaskId,
+    firstIncompleteTaskRef,
+  ])
+
   return (
-    <TasksContext.Provider
-      value={{
-        tasks,
-        filteredTasks,
-        firstIncompleteTaskId,
-        firstIncompleteTaskRef,
-        deleteTask,
-        deleteAllTasks,
-        toggleTaskComplete,
-        newTaskTitle,
-        setNewTaskTitle,
-        searchQuery,
-        setSearchQuery,
-        newTaskInputRef,
-        addTask,
-        disappearingTaskId,
-        appearingTaskId,
-      }}
-    >
+    <TasksContext.Provider value={value}>
       {children}
     </TasksContext.Provider>
   )
